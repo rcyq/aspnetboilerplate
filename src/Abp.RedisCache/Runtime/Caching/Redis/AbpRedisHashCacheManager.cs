d60490a8c58e296606d990092ae2e256092c0f5b
ryancyq <quan0203@gmail.com>
2020-01-03 00:13:19 +0800

    add AbpRedisHashCacheManager

---
+++ src/Abp.RedisCache/Runtime/Caching/Redis/AbpRedisHashCacheManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Abp.Dependency;
+using Abp.Runtime.Caching.Configuration;
+
+namespace Abp.Runtime.Caching.Redis
+{
+    /// <summary>
+    /// Used to create <see cref="AbpRedisHashCache"/> instances.
+    /// </summary>
+    public class AbpRedisHashCacheManager : CacheManagerBase<IAbpRedisHashCache>
+    {
+        private readonly IIocManager _iocManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbpRedisHashCacheManager"/> class.
+        /// </summary>
+        public AbpRedisHashCacheManager(IIocManager iocManager, ICachingConfiguration configuration) 
+            : base(configuration)
+        {
+            _iocManager = iocManager;
+            _iocManager.RegisterIfNot<AbpRedisHashCache>(DependencyLifeStyle.Transient);
+        }
+
+        protected override IAbpRedisHashCache CreateCacheImplementation(string name)
+        {
+            return _iocManager.Resolve<AbpRedisHashCache>(new { name });
+        }
+
+        protected override void DisposeCaches()
+        {
+            foreach (var cache in Caches)
+            {
+                _iocManager.Release(cache.Value);
+            }
+        }
+    }
+}
